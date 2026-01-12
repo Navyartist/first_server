2bb48d32d97b2b6d9e26065d7253f1dead15e67c
Navyartist <navyartist7@gmail.com>
2026-01-12 12:02:48 +0900

    html: 직접 assemble 작업해보기

--- training.py
+++ training.py
@@ -1,1 +1,23 @@
-body = '공미남'
+
+class Body():
+    def __init__(self, content):
+        self.content = content
+
+
+practics= {
+    'left_angle_bracket' : '<',
+    'right_angle_bracket' : '>',
+    'tag_name' : '',
+    'close_tag_slash': '/',
+    'children_elements': []
+}
+
+animal = '표범'
+content = '고양이과'
+
+result = practics['left_angle_bracket'] + animal + practics['right_angle_bracket'] \
++ content \
++ practics['left_angle_bracket'] + practics['close_tag_slash'] + animal + practics['right_angle_bracket'] 
+
+a = Body('치타')
+print(result)
