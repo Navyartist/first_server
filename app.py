0a1d16d3006b53e573a355287f4e0c8f69f099a7
Navyartist <navyartist7@gmail.com>
2026-01-12 15:03:33 +0900

    html파일 -> html 파일 링크만들기 (<a href='파일명'> 태그)

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     # 파이썬에서 만든 데이터
     user_name = "Gemini"
-    status = "연결 성공!"
+    status = "연결 성공!asdf"
     
     # render_template이 index.html을 찾아 데이터를 넣어줍니다.
     return render_template('index.html', name=user_name, msg=status)
